69c7e4da4240173eba6e6ee2f7bf5a6e03a0d92d
Bruno Saraiva <45214467+bdfsaraiva@users.noreply.github.com>
2022-10-13 00:46:28 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,94 @@
+from setuptools import setup, find_packages
+
+test_packages = [
+    "pytest>=5.4.3",
+    "pytest-cov>=2.6.1"
+]
+
+docs_packages = [
+    "mkdocs>=1.1",
+    "mkdocs-material>=4.6.3",
+    "mkdocstrings>=0.8.0",
+]
+
+base_packages = [
+    "numpy>=1.20.0",
+    "hdbscan>=0.8.28",
+    "umap-learn>=0.5.0",
+    "pandas>=1.1.5",
+    "scikit-learn>=0.22.2.post1",
+    "tqdm>=4.41.1",
+    "sentence-transformers>=0.4.1",
+    "plotly>=4.7.0",
+    "pyyaml<6.0"
+]
+
+flair_packages = [
+    "transformers>=3.5.1",
+    "torch>=1.4.0",
+    "flair>=0.7"
+]
+
+spacy_packages = [
+    "spacy>=3.0.1"
+]
+
+use_packages = [
+    "tensorflow",
+    "tensorflow_hub",
+    "tensorflow_text"
+]
+
+gensim_packages = [
+    "gensim>=4.0.0"
+]
+
+extra_packages = flair_packages + spacy_packages + use_packages + gensim_packages
+
+dev_packages = docs_packages + test_packages + extra_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="py-participation-shifts",
+    packages=find_packages(exclude=["notebooks", "docs"]),
+    version="0.12.0",
+    author="Bruno D. Ferreira-Saraiva",
+    author_email="bruno.saraiva@ulusofona.pt",
+    description="py Participation Shift description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bdfsaraiva/py-Participation-Shifts/",
+    project_urls={
+        "Documentation": "https://bdfsaraiva.github.io/py-Participation-Shifts/",
+        "Source Code": "https://github.com/bdfsaraiva/py-Participation-Shifts/",
+    },
+    keywords="nlp sbert embeddings participation-shifts",
+    classifiers=[
+        "Programming Language :: Python",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=base_packages,
+    extras_require={
+        "test": test_packages,
+        "docs": docs_packages,
+        "dev": dev_packages,
+        "flair": flair_packages,
+        "spacy": spacy_packages,
+        "use": use_packages,
+        "gensim": gensim_packages
+    },
+    python_requires='>=3.7',
+)
