b555c34207b29cdc05cc68c264b342c2859daea7
Bruno Saraiva <45214467+bdfsaraiva@users.noreply.github.com>
2022-10-13 01:17:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,52 +1,5 @@
 from setuptools import setup, find_packages
 
-test_packages = [
-    "pytest>=5.4.3",
-    "pytest-cov>=2.6.1"
-]
-
-docs_packages = [
-    "mkdocs>=1.1",
-    "mkdocs-material>=4.6.3",
-    "mkdocstrings>=0.8.0",
-]
-
-base_packages = [
-    "numpy>=1.20.0",
-    "hdbscan>=0.8.28",
-    "umap-learn>=0.5.0",
-    "pandas>=1.1.5",
-    "scikit-learn>=0.22.2.post1",
-    "tqdm>=4.41.1",
-    "sentence-transformers>=0.4.1",
-    "plotly>=4.7.0",
-    "pyyaml<6.0"
-]
-
-flair_packages = [
-    "transformers>=3.5.1",
-    "torch>=1.4.0",
-    "flair>=0.7"
-]
-
-spacy_packages = [
-    "spacy>=3.0.1"
-]
-
-use_packages = [
-    "tensorflow",
-    "tensorflow_hub",
-    "tensorflow_text"
-]
-
-gensim_packages = [
-    "gensim>=4.0.0"
-]
-
-extra_packages = flair_packages + spacy_packages + use_packages + gensim_packages
-
-dev_packages = docs_packages + test_packages + extra_packages
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -81,14 +34,14 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     install_requires=base_packages,
-    extras_require={
-        "test": test_packages,
-        "docs": docs_packages,
-        "dev": dev_packages,
-        "flair": flair_packages,
-        "spacy": spacy_packages,
-        "use": use_packages,
-        "gensim": gensim_packages
-    },
-    python_requires='>=3.7',
+#     extras_require={
+#         "test": test_packages,
+#         "docs": docs_packages,
+#         "dev": dev_packages,
+#         "flair": flair_packages,
+#         "spacy": spacy_packages,
+#         "use": use_packages,
+#         "gensim": gensim_packages
+#     },
+    python_requires='>=3.8',
 )
