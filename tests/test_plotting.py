da21960be0f8f38853b241b61da60c2d483663c8
Bruno Saraiva <bdsaraiva@hotmail.com>
2023-03-21 14:49:42 +0000

    Add tests to get 100% cov in plotting module

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import pytest
 
 from parshift import cond_probs, frequency_treemap
 
@@ -11,3 +12,11 @@ def test_frequency_treemap(pshift_freq_table):
     assert type(ax) == type(
         frequency_treemap(conditional_probabilities_df, column_name="pshift_type")
     )
+
+
+def test_frequency_treemap_errors(pshift_freq_table):
+    conditional_probabilities_df = cond_probs(pshift_freq_table["df_ps"])
+    with pytest.raises(TypeError):
+        frequency_treemap(conditional_probabilities_df, column_name=1)
+    with pytest.raises(ValueError):
+        frequency_treemap(conditional_probabilities_df, column_name="Bye")
