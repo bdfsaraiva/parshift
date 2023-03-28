202dbce25b5355b9384a023e5fcdbc22d0bb7fe4
Nuno Fachada <nuno.fachada@ulusofona.pt>
2023-03-28 18:16:22 +0100

    Fix name of license file in source code license headers

--- parshift/plotting.py
+++ parshift/plotting.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2022-2023 Bruno Saraiva and contributors
-# Distributed under the MIT License (See accompanying file LICENSE.txt or copy
+# Distributed under the MIT License (See accompanying file LICENSE or copy
 # at http://opensource.org/licenses/MIT)
 
 import matplotlib
