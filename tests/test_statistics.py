f3a333b6293af61586d6e0b5da129b416625b2e7
Nuno Fachada <nuno.fachada@ulusofona.pt>
2023-03-28 18:28:58 +0100

    Parameterize cond_prob() errors test

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -16,15 +16,6 @@ def test_frequency_table(pshift_freq_table):
     assert result[0] == pshift_freq_table["freq_table"]
 
 
-def test_cond_probs_errors():
-    with pytest.raises(TypeError):
-        cond_probs(1)
-    with pytest.raises(TypeError):
-        cond_probs("Bye")
-    with pytest.raises(TypeError):
-        cond_probs(dict())
-
-
 def test_cond_probs_ok(pshift_freq_table):
     result = cond_probs(pshift_freq_table["df_ps"])
     assert isinstance(result, pd.DataFrame)
@@ -35,3 +26,11 @@ def test_cond_probs_ok(pshift_freq_table):
         "CP",
         "CPeTC",
     ]
+
+
+@pytest.mark.parametrize(
+    "pscodes,expecterr", [(1, TypeError), ("Bye", TypeError), ({}, TypeError)]
+)
+def test_cond_probs_errors(pscodes, expecterr):
+    with pytest.raises(expecterr):
+        cond_probs(pscodes)
